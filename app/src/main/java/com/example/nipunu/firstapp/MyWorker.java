d1873b4d4bc837f5542d6281740dc65b74dba87a
Nipunu Wijesingha <nipunu.wijesingha@gmail.com>
2019-06-14 12:32:00 +0530

    Initial commit of the class added the method - doubleTheValue

---
+++ app/src/main/java/com/example/nipunu/firstapp/MyWorker.java
@@ -0,0 +1,10 @@
+package com.example.nipunu.firstapp;
+
+/**
+ * Created by Nipunu on 14,June,2019
+ */
+public class MyWorker {
+    public static int doubleTheValue(int value){
+        return value * 2;
+    }
+}
